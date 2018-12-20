0ec6b581ac839a0deb171b288de90af933dac030
Carolyna <Carolynacaarolmantovani@hotmail.com>
2018-12-20 14:37:35 -0200

    Inclusão do Endereço, Cliente e mapeamento entre as tabelas

--- src/main/java/com/example/LojaApplication.java
+++ src/main/java/com/example/LojaApplication.java
@@ -10,10 +10,15 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import com.example.domain.Categoria;
 import com.example.domain.Cidade;
+import com.example.domain.Cliente;
+import com.example.domain.Endereco;
 import com.example.domain.Estado;
 import com.example.domain.Produto;
+import com.example.domain.enums.TipoCliente;
 import com.example.repositories.CategoriaRepository;
 import com.example.repositories.CidadeRepository;
+import com.example.repositories.ClienteRepository;
+import com.example.repositories.EnderecoRepository;
 import com.example.repositories.EstadoRepository;
 import com.example.repositories.ProdutoRepository;
 
@@ -31,6 +36,12 @@ public class LojaApplication implements CommandLineRunner {
 
 	@Autowired
 	private EstadoRepository estadoRepository;
+
+	@Autowired
+	private ClienteRepository clienteRepository;
+
+	@Autowired
+	private EnderecoRepository enderecoRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(LojaApplication.class, args);
@@ -69,6 +80,18 @@ public class LojaApplication implements CommandLineRunner {
 
 		estadoRepository.saveAll(Arrays.asList(est1, est2));
 		cidadeRepository.saveAll(Arrays.asList(c1, c2, c3));
+
+		Cliente cli1 = new Cliente(null, "Maria Silva", "maria@gmail.com", "3637892377", TipoCliente.PESSOAFISICA);
+
+		cli1.getTelefones().addAll(Arrays.asList("32654587", "123468424"));
+
+		Endereco e1 = new Endereco(null, "Rua Flores", "300", "Apto 303", "Jardim", "568989", cli1, c1);
+		Endereco e2 = new Endereco(null, "Av Matos", "105", "Sala 108", "Centro", "7851414", cli1, c2);
+
+		cli1.getEndereços().addAll(Arrays.asList(e1, e2));
+
+		clienteRepository.saveAll(Arrays.asList(cli1));
+		enderecoRepository.saveAll(Arrays.asList(e1, e2));
 	}
 
 }
