6e137a3670e79fa1fe1d397fb68964a8df2d4343
Carolyna <Carolynacaarolmantovani@hotmail.com>
2018-12-20 09:47:50 -0200

    Conexão com o banco de dados ok

--- src/main/java/com/example/domain/Categoria.java
+++ src/main/java/com/example/domain/Categoria.java
@@ -6,8 +6,10 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.Table;
 
 @Entity
+@Table(name = "Categoria")
 public class Categoria implements Serializable {
 	private static final long serialVersionUID = 1L;
 
@@ -40,7 +42,8 @@ public class Categoria implements Serializable {
 	public void setNome(String nome) {
 		this.nome = nome;
 	}
-
+	
+	
 	@Override
 	public int hashCode() {
 		final int prime = 31;
@@ -48,8 +51,6 @@ public class Categoria implements Serializable {
 		result = prime * result + ((id == null) ? 0 : id.hashCode());
 		return result;
 	}
-
-	/* Metodo de comparação entre o ID */
 	
 	@Override
 	public boolean equals(Object obj) {
