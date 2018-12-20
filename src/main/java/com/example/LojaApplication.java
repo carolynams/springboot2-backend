d34de76267403eef1331782d5b7676cf32567ab2
Carolyna <Carolynacaarolmantovani@hotmail.com>
2018-12-20 10:21:32 -0200

    Operação de instanciação do banco

--- src/main/java/com/example/LojaApplication.java
+++ src/main/java/com/example/LojaApplication.java
@@ -1,13 +1,36 @@
 package com.example;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class LojaApplication {
+import com.example.domain.Categoria;
+import com.example.repositories.CategoriaRepository;
 
+@SpringBootApplication
+public class LojaApplication implements CommandLineRunner{
+
+	@Autowired
+	private CategoriaRepository categoriaRepository;
+	
 	public static void main(String[] args) {
 		SpringApplication.run(LojaApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		// TODO Auto-generated method stub
+		
+		Categoria cat1 = new Categoria(null, "Informática");
+		Categoria cat2 = new Categoria(null, "Escritório");
+		
+		categoriaRepository.saveAll(Arrays.asList(cat1, cat2));
+		
+		
 	}
 
 }
