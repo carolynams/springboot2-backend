6d220b72a5f1625a413210bf9657b03edeff348c
Carolyna <Carolynacaarolmantovani@hotmail.com>
2018-12-20 12:00:41 -0200

    Tratamento de exceção de busca

--- src/main/java/com/example/services/CategoriaService.java
+++ src/main/java/com/example/services/CategoriaService.java
@@ -7,18 +7,18 @@ import org.springframework.stereotype.Service;
 
 import com.example.domain.Categoria;
 import com.example.repositories.CategoriaRepository;
+import com.example.services.exceptions.ObjectNotFoundException;
 
 @Service
 public class CategoriaService {
-	
+
 	@Autowired
 	private CategoriaRepository categoriaRepository;
 
 	public Categoria buscar(Integer id) {
 		Optional<Categoria> objeto = categoriaRepository.findById(id);
-		
-		return objeto.orElse(null);
-		
+		return objeto.orElseThrow(() -> new ObjectNotFoundException(
+				"Objeto não encontrado! Id: " + id + ", Tipo: " + Categoria.class.getName()));
 
 	}
 }
