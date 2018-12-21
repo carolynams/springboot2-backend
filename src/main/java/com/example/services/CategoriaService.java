7109deed7476f4862b3538afc42b9c49f4183672
Carolyna <Carolynacaarolmantovani@hotmail.com>
2018-12-21 10:25:56 -0200

    DELETE

--- src/main/java/com/example/services/CategoriaService.java
+++ src/main/java/com/example/services/CategoriaService.java
@@ -3,10 +3,12 @@ package com.example.services;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Service;
 
 import com.example.domain.Categoria;
 import com.example.repositories.CategoriaRepository;
+import com.example.services.exceptions.DataIntegrityException;
 import com.example.services.exceptions.ObjectNotFoundException;
 
 @Service
@@ -30,5 +32,14 @@ public class CategoriaService {
 	public Categoria update(Categoria objeto) {
 		find(objeto.getId());
 		return categoriaRepository.save(objeto);
+	}
+
+	public void delete(Integer id) {
+		find(id);
+		try {
+			categoriaRepository.deleteById(id);
+		} catch (DataIntegrityViolationException e) {
+			throw new DataIntegrityException("Não é possível excluir uma categoria que possui produtos");
+		}
 	}
 }
